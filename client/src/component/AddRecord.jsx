2f1198316461caa934324e7cddddd82708c8515a
sagarwalke-dev <sagarwalke7030@gmail.com>
2021-12-01 04:48:15 -0800

    Add Record UI

--- client/src/component/AddRecord.jsx
+++ client/src/component/AddRecord.jsx
@@ -4,59 +4,118 @@ import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import { Button, Card, TextField } from "@mui/material";
 import TimePicker from "@mui/lab/TimePicker";
+import DatePicker from "@mui/lab/DatePicker";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import Grid from '@mui/material/Grid';
+import { Col, Form, FormGroup, Label, Input, FormText, Row } from 'reactstrap';
+
 
 function AddRecord() {
   const [startTime, setStartTime] = React.useState(
     new Date("2014-08-18T21:11:54")
   );
   const [endTime, setEndTime] = React.useState(new Date("2014-08-18T21:11:54"));
-  const handleStartTimeChange = (newValue) => {
-    setStartTime(newValue);
-  };
-  const handleEndTimeChange = (newValue) => {
-    setEndTime(newValue);
-  };
+  const [date, setDate] = React.useState(new Date("2014-08-18T21:11:54"));
+  const [amount,setAmount ] = React.useState("0");
+  const [description, setDescription] = React.useState(" ");
 
+  const handleAmount=(e)=>{
+    setAmount(e.target.value);
+  }
+  const handleDescription=(e)=>{
+    setDescription(e.target.value);
+  }
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(startTime);
-    console.log(endTime);
+ const data={
+   "date":date,
+   "startTime":startTime,
+   "endTime":endTime,
+   "amount":amount,
+   "description":description
+ }
+ console.log(data);
   };
+
   return (
     <>
-      <CssBaseline />
-      <Container maxWidth='lg'>
-        <Card variant='outlined'>
-          <Box
-            sx={{
-              height: "100vh",
-              textAlign: "center",
-            }}>
-            <h1>Add Record</h1>
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <Container className='card card-signin flex-row my-5 shadow-lg p-3 mb-5 bg-white rounded text-center' >
+          <h1>Add Record </h1>
+          <Row>
+
+            <Col md={3} className='mt-3'>
               <TimePicker
-                label='Start Time'
+                renderInput={(props) => <TextField {...props} />}
+                label="Start Time"
                 value={startTime}
-                onChange={handleStartTimeChange}
-                renderInput={(params) => <TextField {...params} />}
+                onChange={(newValue) => {
+                  setStartTime(newValue);
+                }}
               />
+            </Col>
+            <Col md={3} className='mt-3'>
 
               <TimePicker
-                label='End Time'
+                renderInput={(props) => <TextField {...props} />}
+                label="End Time"
                 value={endTime}
-                onChange={handleEndTimeChange}
-                renderInput={(params) => <TextField {...params} />}
+                onChange={(newValue) => {
+                  setEndTime(newValue);
+                }}
               />
+            </Col>
 
-              <Button variant='contained' onClick={handleSubmit}>
-                Submit
-              </Button>
-            </LocalizationProvider>
-          </Box>
-        </Card>
-      </Container>
+
+
+          </Row>
+
+          <Row className='mt-3'>
+            <Col md={3} className='mt-3'>
+              <DatePicker
+                label="Working Day Date"
+                value={date}
+                onChange={(newValue) => {
+                  setDate(newValue);
+                }}
+                renderInput={(params) => (
+                  <TextField {...params} helperText={params?.inputProps?.placeholder} />
+                )}
+              />
+            </Col>
+            <Col md={3} className='mt-3'>
+
+              <TextField
+                id="outlined-number"
+                label="Number"
+                type="number"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                onChange={handleAmount}
+              />
+            </Col>
+
+          </Row>
+
+          <Row className='mt-3'>
+            <Col md={3} className='mt-3'>
+              <TextField
+                id="filled-helperText"
+                label="Work Description"
+                defaultValue=" "
+                onChange={handleDescription}
+              />
+            </Col>
+            <Col md={3} className='mt-3'>
+              <Button variant="contained" onClick={handleSubmit}>Add Record</Button>
+
+            </Col>
+          </Row>
+
+        </Container>
+      </LocalizationProvider>
     </>
   );
 }
