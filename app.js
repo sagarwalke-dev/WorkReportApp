9ab19395c1c4629f4eee1de1e44bb53a007b6fdd
Sagar <sagarwalke.web@gmail.com>
2021-12-01 21:13:34 +0530

    CORS issue fixed

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const app = express();
 const path = require("path");
 const recordRouter = require("./api/user/routes/recordRouter");
 const db = require("./api/services/database");
-const cros =require('cros');
+// const cros =require('cros');
 const configEnv = require("dotenv").config({
   path: path.join(__dirname, "./config.env"),
 });
 
 app.use(express.json());
-app.use(cros());
+// app.use(cros());
 //configure router middleware here
 app.use("/api/record/", recordRouter);
 
